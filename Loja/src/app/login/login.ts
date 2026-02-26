189789d3ce6fa5fce3df41a7ab2a77a7c44ef456
JDanielMonteiro <joisondaniel2@gmail.com>
2026-02-25 21:33:22 -0300

    Finalizndo tela de login e adicionando rota de cadastro

--- Loja/src/app/login/login.ts
+++ Loja/src/app/login/login.ts
@@ -8,34 +8,60 @@ import {FormControl, FormsModule, ReactiveFormsModule, Validators} from '@angula
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {merge} from 'rxjs';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.html',
   styleUrl: './login.css',
-  imports: [MatCardModule, MatChipsModule, MatProgressBarModule, MatFormFieldModule, MatInputModule, FormsModule, ReactiveFormsModule],
+  imports: [MatCardModule, MatChipsModule, MatProgressBarModule, MatFormFieldModule, MatInputModule, 
+    FormsModule, ReactiveFormsModule, MatButtonModule,MatIconModule, RouterModule],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Login {
-  longText = 'Acesse e aproveite uma experiência segura dentro da Motosbr.'
 
   readonly email = new FormControl('', [Validators.required, Validators.email]);
+  readonly password = new FormControl('', [Validators.required]);
 
-  errorMessage = signal('');
+  errorMessageEmail = signal('');            // variáveis golbais no front não começam com letra maiúscula
+  errorMessagePassword = signal('');
+  hide = signal(true);
 
   constructor() {
     merge(this.email.statusChanges, this.email.valueChanges)
       .pipe(takeUntilDestroyed())
-      .subscribe(() => this.updateErrorMessage());
+      .subscribe(() => this.updateErrorMessageEmail());
+
+      merge(this.password.statusChanges, this.password.valueChanges)
+      .pipe(takeUntilDestroyed())
+      .subscribe(() => this.updateErrorMessagePassword());
   }
 
-  updateErrorMessage() {
+  updateErrorMessageEmail() {
     if (this.email.hasError('required')) {
-      this.errorMessage.set('Campo obrigatório. Informe um e-mail válido.');
+      this.errorMessageEmail.set('Campo obrigatório. Informe um e-mail válido.');
     } else if (this.email.hasError('email')) {
-      this.errorMessage.set('Não é um email valido');
+      this.errorMessageEmail.set('Não é um email valido');
     } else {
-      this.errorMessage.set('');
+      this.errorMessageEmail.set('');
     }
+  }
+
+  updateErrorMessagePassword() {
+    if (this.password.hasError('required')) {
+      this.errorMessagePassword.set('Campo obrigatório.');
+    } else {
+      this.errorMessagePassword.set('');
+    }
+  }
+
+  
+  clickEvent(event: MouseEvent) {
+    this.hide.set(!this.hide());
+    event.stopPropagation();
   }
 }
